e6cffdb0aa85254f43cf6de98cfc9d815be741ed
--global <--global>
2023-11-28 14:58:14 +0800

    add icp code

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -19,6 +19,14 @@ const show = ref(false)
       </span>
     </HoverButton>
 
+
     <Setting v-if="show" v-model:visible="show" />
+  </footer>
+  <footer class="flex items-center justify-between min-w-0 p-4 overflow-hidden border-t dark:border-neutral-800">
+    <div class="flex items-center overflow-hidden">
+      <p class="overflow-hidden text-xs text-gray-500 text-ellipsis whitespace-nowrap">
+        <a href="https://beian.miit.gov.cn">沪ICP备2023034202号-1</a>
+      </p>
+    </div>
   </footer>
 </template>
